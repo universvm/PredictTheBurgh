89c4bc027962556b2357a20ea03628483fcfd4d7
Leonardo Castorina <leonardo.castorina98@gmail.com>
2019-03-16 18:10:24 +0000

    Remove double dropna

--- main.py
+++ main.py
@@ -5,10 +5,5 @@ from processing.data_processing import load_data_df
 if __name__ == '__main__':
     stock_data = load_data_df(DATA_PATH, separator=SEP,
                               date_column=DATE_COLUMN)
-
-    # print(stock_data[stock_data.columns[7]])
-    print(stock_data.info())
-    stock_data.dropna(inplace=True)
-    # print(stock_data[stock_data.columns[7]])
     print(stock_data.info())
 
