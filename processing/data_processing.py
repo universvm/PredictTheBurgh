0908b16437cb2425a45b51050902d961a406471b
Leonardo Castorina <leonardo.castorina98@gmail.com>
2019-03-16 23:27:10 +0000

    Add separator

--- processing/data_processing.py
+++ processing/data_processing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def load_data_df(dataset_dir, separator='            ', date_column='date'):
+def load_data_df(dataset_dir, separator=',', date_column='date'):
     loaded_df = pd.read_csv(dataset_dir, sep=separator)
     loaded_df.drop_duplicates(subset=date_column, inplace=True)
     loaded_df.dropna(inplace=True)
